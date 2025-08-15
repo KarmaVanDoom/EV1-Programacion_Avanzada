18564a0b61198fac7ac77329f74736d995561655
KarmaVanDoom <karmacastillo2017@gmail.com>
2025-08-15 17:17:19 -0400

    Implementación de los cálculo y validación

--- CalculadoraDePromedios/CalculadoraDePromedios/Helpers/Calculos.cs
+++ CalculadoraDePromedios/CalculadoraDePromedios/Helpers/Calculos.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System; 
 
 namespace CalculadoraDePromedios.Helpers
 {
-    internal class Calculos
+    public static class Calculos
     {
+        
+        private static float CalcularPromedioPonderado(float c1, int p1, float c2, int p2, float c3, int p3, float c4, int p4)
+        {
+            // La "f" es importante para decirle a C# que son números de tipo float
+            float promedio = (c1 * p1 / 100f) + (c2 * p2 / 100f) + (c3 * p3 / 100f) + (c4 * p4 / 100f);
+            return promedio;
+        }
+
+       
+        public static float CalcularPromedioFinal(float c1, int p1, float c2, int p2, float c3, int p3, float c4, int p4, float notaExamen)
+        {
+            float promedioPonderado = CalcularPromedioPonderado(c1, p1, c2, p2, c3, p3, c4, p4);
+            float promedioFinal = (promedioPonderado * 0.7f) + (notaExamen * 0.3f);
+
+            // Redondeamos el resultado a 1 decimal
+            return (float)Math.Round(promedioFinal, 1);
+        }
+
+
+        public static float CalcularNotaExamenNecesaria(float c1, int p1, float c2, int p2, float c3, int p3, float c4, int p4)
+        {
+            float promedioPonderado = CalcularPromedioPonderado(c1, p1, c2, p2, c3, p3, c4, p4);
+
+            
+        
+            float notaNecesaria = (4.0f - (promedioPonderado * 0.7f)) / 0.3f;
+
+            return (float)Math.Round(notaNecesaria, 1);
+        }
     }
 }
--- CalculadoraDePromedios/CalculadoraDePromedios/Helpers/Validaciones.cs
+++ CalculadoraDePromedios/CalculadoraDePromedios/Helpers/Validaciones.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CalculadoraDePromedios.Helpers
+﻿namespace CalculadoraDePromedios.Helpers
 {
-    internal class Validaciones
+    public static class Validaciones
     {
+        
+        public static bool ValidarCalificacion(float calificacion)
+        {
+            if (calificacion >= 1.0f && calificacion <= 7.0f)
+            {
+                return true; // Es válida
+            }
+            return false; // No es válida
+        }
+
+        
+        public static bool SumaPonderacionEsCien(int p1, int p2, int p3, int p4)
+        {
+            if ((p1 + p2 + p3 + p4) == 100)
+            {
+                return true; // Es correcta
+            }
+            return false; // Es incorrecta
+        }
     }
 }
